5de17fc14069e7c9b15031f9d157d058f19855f0
Darya Matveeva <darya.d.chernikova@gmail.com>
2022-12-06 14:35:50 +0300

    init project

---
+++ src/main/java/ru/dmatveeva/model/Vehicle.java
@@ -0,0 +1,36 @@
+package ru.dmatveeva.model;
+
+import java.math.BigDecimal;
+
+public class Vehicle {
+
+    private BigDecimal costUsd;
+
+    private int mileage;
+
+    private int productionYear;
+
+    public BigDecimal getCostUsd() {
+        return costUsd;
+    }
+
+    public void setCostUsd(BigDecimal costUsd) {
+        this.costUsd = costUsd;
+    }
+
+    public int getMileage() {
+        return mileage;
+    }
+
+    public void setMileage(int mileage) {
+        this.mileage = mileage;
+    }
+
+    public int getProductionYear() {
+        return productionYear;
+    }
+
+    public void setProductionYear(int productionYear) {
+        this.productionYear = productionYear;
+    }
+}
